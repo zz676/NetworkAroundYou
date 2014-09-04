2a991382cc8ad3ffc85c32f5b7a7942bec7ecc9a
unknown <zz676@nyu.edu>
2014-09-03 22:30:40 -0400

    Moved here from my local repo

---
+++ src/edu/nyu/ga2620/network/network/OsFingerprint.java
@@ -0,0 +1,14 @@
+/*
+ * Zhisheng Zhou
+ */
+
+package edu.nyu.ga2620.network.network;
+
+public class OsFingerprint {
+
+    // private final static String TAG = "OsFingerprint";
+
+    public static String finger(long[] longArray) {
+        return null;
+    }
+}
---
+++ src/edu/nyu/ga2620/network/utils/Help.java
@@ -0,0 +1,17 @@
+/*
+ * Zhisheng Zhou
+ */
+
+package edu.nyu.ga2620.network.utils;
+
+import edu.nyu.ga2620.network.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Help extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
